e314a47c1964525ff971604d6e4f9b1cdd8f7af9
Hakkim Appas Manthiri <hakkimappas333@gmail.com>
2026-09-12 18:09:21 +0530

    NovaVault Initial Commit

--- server/src/controllers/webhookController.js
+++ server/src/controllers/webhookController.js
@@ -25,8 +25,6 @@ const verifyWebhookSignature = (rawBody, signature, secret) => {
 
 const handleRazorpayWebhook = async (req, res, next) => {
   try {
-    console.log("RAZORPAY WEBHOOK REQUEST RECEIVED");
-
     const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET;
     const signature = req.headers["x-razorpay-signature"];
     const eventId = req.headers["x-razorpay-event-id"];
